8da5c19bbb308feb5d5b7b35ddd7a90e4fdb4fb8
Frank0587 <bhunjimko@web.de>
2024-09-22 20:37:13 +0200

    SP-v12: Flap mixer to elevator

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -7,7 +7,7 @@
 #include "ap_version.h"
 
 // SP-Master-branch
-#define THISFIRMWARE "ArduPlane V4.5.6-SP-v11"
+#define THISFIRMWARE "ArduPlane V4.5.6-SP-v12"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,6,FIRMWARE_VERSION_TYPE_OFFICIAL
