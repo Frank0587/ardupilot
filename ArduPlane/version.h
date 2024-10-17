dc1dcc2103bbfa78547795791ff93488c7857592
Frank0587 <bhunjimko@web.de>
2024-10-17 08:36:04 +0200

    SP-v14: Landing Gear slow down

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -7,7 +7,7 @@
 #include "ap_version.h"
 
 // SP-Master-branch
-#define THISFIRMWARE "ArduPlane V4.6.0-SP-v13"
+#define THISFIRMWARE "ArduPlane V4.6.0-SP-v14"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,0,FIRMWARE_VERSION_TYPE_DEV
