90273ab08f1531306381c9a2695f40949b8645af
Frank0587 <bhunjimko@web.de>
2025-02-26 14:03:48 +0100

    update offline mavlink includes again

--- .FeatGit/build/SpeedyBeeF405WING/libraries/GCS_MAVLink/include/mavlink/v2.0/minimal/minimal.h
+++ .FeatGit/build/SpeedyBeeF405WING/libraries/GCS_MAVLink/include/mavlink/v2.0/minimal/minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MINIMAL_XML_HASH 8029689906901525042
+#define MAVLINK_MINIMAL_XML_HASH 742808027714953718
 
 #ifdef __cplusplus
 extern "C" {
--- .FeatGit/build/SpeedyBeeF405WING/libraries/GCS_MAVLink/include/mavlink/v2.0/ualberta/ualberta.h
+++ .FeatGit/build/SpeedyBeeF405WING/libraries/GCS_MAVLink/include/mavlink/v2.0/ualberta/ualberta.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_UALBERTA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_UALBERTA_XML_HASH 574924627662839675
+#define MAVLINK_UALBERTA_XML_HASH 6351335102056592965
 
 #ifdef __cplusplus
 extern "C" {
