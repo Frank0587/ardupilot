126b5bb26bdba6cc9a0e5ffc826fec8f20a664fc
Frank0587 <bhunjimko@web.de>
2025-11-18 17:58:44 +0100

    update offline mavlink includes, VSC tasks für GIT und Ardupilot (optional)

--- .FeatGit/build/SpeedyBeeF405WING/libraries/GCS_MAVLink/include/mavlink/v2.0/icarous/icarous.h
+++ .FeatGit/build/SpeedyBeeF405WING/libraries/GCS_MAVLink/include/mavlink/v2.0/icarous/icarous.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ICAROUS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ICAROUS_XML_HASH -7207890420075968254
+#define MAVLINK_ICAROUS_XML_HASH 1486195149509301008
 
 #ifdef __cplusplus
 extern "C" {
--- .FeatGit/build/SpeedyBeeF405WING/libraries/GCS_MAVLink/include/mavlink/v2.0/test/test.h
+++ .FeatGit/build/SpeedyBeeF405WING/libraries/GCS_MAVLink/include/mavlink/v2.0/test/test.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_TEST.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_TEST_XML_HASH 7615001216846104087
+#define MAVLINK_TEST_XML_HASH -3274739093548347871
 
 #ifdef __cplusplus
 extern "C" {
