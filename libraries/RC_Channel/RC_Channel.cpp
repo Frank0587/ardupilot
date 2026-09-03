33dc13f4f2dae0b483c1812715cf25cce8a97906
Frank0587 <bhunjimko@web.de>
2026-09-03 10:07:13 +0200

    SP-v16: optional 2nd switch function, activated when 3pos input is LOW, disable OSD screens 5+6 when armed

--- libraries/RC_Channel/RC_Channel.cpp
+++ libraries/RC_Channel/RC_Channel.cpp
@@ -267,6 +267,13 @@ const AP_Param::GroupInfo RC_Channel::var_info[] = {
     // @User: Standard
     AP_GROUPINFO("OPTION",  6, RC_Channel, option, 0),
 
+    // @Param: OPTION2
+    // @DisplayName: RC input option2, activated when Input switch set to low 
+    // @Description: 2nd Function assigned to this RC channel. You can use a 3pos switch to activate two different functions. OPTION at HIGH and OPTION2 at LOW position.
+    // @SortValues: AlphabeticalZeroAtTop
+    // @User: Standard
+    AP_GROUPINFO_FRAME("OPTION2",  7, RC_Channel, option2, 0, AP_PARAM_FRAME_COPTER|AP_PARAM_FRAME_ROVER|AP_PARAM_FRAME_PLANE|AP_PARAM_FRAME_BLIMP),
+
     AP_GROUPEND
 };
 
@@ -976,6 +983,7 @@ const char *RC_Channel::string_for_aux_pos(AuxSwitchPos pos) const
 bool RC_Channel::read_aux()
 {
     const AUX_FUNC _option = (AUX_FUNC)option.get();
+    const AUX_FUNC _option2 = (AUX_FUNC)option2.get();
     if (_option == AUX_FUNC::DO_NOTHING) {
         // may wish to add special cases for other "AUXSW" things
         // here e.g. RCMAP_ROLL etc once they become options
@@ -1018,6 +1026,20 @@ bool RC_Channel::read_aux()
 
     // debounced; undertake the action:
     run_aux_function(_option, new_position, AuxFuncTrigger::Source::RC, ch_in);
+
+    if( _option2 != AUX_FUNC::DO_NOTHING) {
+        switch (new_position) {
+            case AuxSwitchPos::HIGH:
+                run_aux_function(_option2, AuxSwitchPos::LOW, AuxFuncTrigger::Source::RC, ch_in); 
+                break;
+            case AuxSwitchPos::LOW:
+                run_aux_function(_option2, AuxSwitchPos::HIGH, AuxFuncTrigger::Source::RC, ch_in); 
+                break;
+            default:
+                run_aux_function(_option2, new_position, AuxFuncTrigger::Source::RC, ch_in); 
+                break;
+        }
+    }
     return true;
 }
 
