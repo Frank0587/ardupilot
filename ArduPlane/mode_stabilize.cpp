72a5a1b6996f1d2b5b7e1ef918e76ac599970096
Frank0587 <bhunjimko@web.de>
2026-09-03 10:04:00 +0200

    SP-V23: Stabilized mode improvments

--- ArduPlane/mode_stabilize.cpp
+++ ArduPlane/mode_stabilize.cpp
@@ -5,6 +5,13 @@ void ModeStabilize::update()
 {
     plane.nav_roll_cd = 0;
     plane.nav_pitch_cd = 0;
+
+    // add STAB_PITCH_DOWN as target when no stick input        
+    plane.adjust_nav_pitch_throttle();
+    if (plane.fly_inverted()) {
+        plane.nav_pitch_cd = -plane.nav_pitch_cd;
+    }
+
 }
 
 void ModeStabilize::run()
